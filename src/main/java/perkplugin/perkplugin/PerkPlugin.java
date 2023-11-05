ac101ba1d54d1a479100701fac6f145062e0ebae
Cameron-Chilvers-UTS <camchilvers123@gmail.com>
2023-11-06 10:58:52 +1100

    Initial Commit

---
+++ src/main/java/perkplugin/perkplugin/PerkPlugin.java
@@ -0,0 +1,17 @@
+package perkplugin.perkplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PerkPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
